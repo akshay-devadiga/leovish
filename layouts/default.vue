2de7833613e57ed3fec40038188b15f159a3f7f3
add11e2 <akshaydevadiga4@gmail.com>
2023-03-05 23:51:48 +0530

    Update images and logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,70 @@
       </v-container>
     </v-main>
     <v-footer>
-      <span>&copy;Leovish interiors & constructions {{ new Date().getFullYear() }}</span>
+      <v-container fluid>
+      <v-row align="center" justify="space-between">
+         <v-col cols="3">
+          <v-card
+      class="mx-auto"
+      tile
+    >
+  
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-bold text-subtitle">Address:</v-list-item-title>
+          <v-list-item-subtitle>
+            S1 Shivam apartment,Samrath Chowk,South shivajinagar,
+          </v-list-item-subtitle>
+          <v-list-item-subtitle>
+            Sangli 416416, Maharashtra.
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+         </v-col>
+          <v-col cols="auto">
+          <v-card
+      class="mx-auto"
+      tile
+    >
+  
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-bold text-subtitle">Mobile:</v-list-item-title>
+          <v-list-item-subtitle>
+            (+91)8459271766
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+         </v-col>
+           <v-col cols="auto">
+          <v-card
+      class="mx-auto"
+      tile
+    >
+  
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title class="font-weight-bold text-subtitle">Email:</v-list-item-title>
+          <v-list-item-subtitle>
+            leovish.in@gmail.com
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
+         </v-col>
+      <v-col cols="12">
+
+  
+      <div class="px-4 py-2 bg-black text-center">
+        {{ new Date().getFullYear() }} — <strong>&copy; Leovish interiors & constructions</strong>
+      </div>
+      </v-col>
+      </v-row>
+     
+      </v-container>
+      
     </v-footer>
   </v-app>
 </template>
@@ -19,6 +82,11 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
+      icons: [
+        'mdi-facebook',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
       items: [
         {
           icon: 'mdi-apps',
--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,13 @@
     <v-col cols="12">
     <v-card
       class="mx-auto py-6"
-      max-width="774"
       flat
     >
       <v-img
         contain
-        height="350"
-        src="lord-shree-ganesh-temple-haripur.webp"
+        height="500"
+        src="/logo.png"
       ></v-img>
-    <div class="text-center text-h3 my-5 font-weight-bold">श्री </div>
-        <div class="text-h5 text-center">
-     COMING SOON
-        </div>
     </v-card>
     </v-col>
   </v-row>
