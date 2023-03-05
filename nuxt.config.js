a6326065592fd09f717c52510e3bb3cbc5e418eb
add11e2 <akshaydevadiga4@gmail.com>
2023-03-05 21:14:16 +0530

    Update vuetify template

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -44,17 +45,14 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+          primary: '#023047',
           accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
+          secondary: '#FFB703',
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
       }
     }
   },
